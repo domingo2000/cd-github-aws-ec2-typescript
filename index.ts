216aa3b691970b499d937a568dd46d246b1ba31d
domingo2000 <domingo.edwards@uc.cl>
2022-05-27 12:42:04 -0400

    initial-commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+import Koa from 'koa';
+import logger from 'koa-logger';
+import koaBody from 'koa-body';
+import Router from 'koa-router';
+
+const PORT = 3000;
+
+const app = new Koa();
+
+app.use(logger());
+
+app.use(koaBody());
+
+app.use( async (ctx, next) => {
+  console.log('######## REQUEST ########');
+  console.log('BODY', ctx.request.body);
+  console.log('PARAMS', ctx.request.query);
+  console.log('HEADERS', ctx.request.headers);
+  console.log('Files', ctx.request.files); // files access
+  console.log('######################');
+  await next();
+});
+
+const router = new Router();
+
+
+router.get('/', (ctx, next) => {
+  ctx.body = { message: "Hello world!" };
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(PORT, () => {
+  console.log("Server is running on port:", PORT)
+})
+
